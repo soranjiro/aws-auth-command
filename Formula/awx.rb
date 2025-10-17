b8f516713e0f985e1ee9a03d75f913bd01ffc55e
soranjiro <yutayuta540@gmail.com>
2025-10-17 19:09:22 +0900

    Add Homebrew formula for binary-only installation

---
+++ Formula/awx.rb
@@ -0,0 +1,33 @@
+class Awx < Formula
+  desc "AWS CLI authentication wrapper for seamless multi-account and MFA workflows"
+  homepage "https://github.com/soranjiro/aws-auth-command"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/soranjiro/aws-auth-command/releases/download/v0.1.0/awx-aarch64-apple-darwin.tar.gz"
+      sha256 "SHA256_MAC_ARM"
+    else
+      url "https://github.com/soranjiro/aws-auth-command/releases/download/v0.1.0/awx-x86_64-apple-darwin.tar.gz"
+      sha256 "SHA256_MAC_INTEL"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/soranjiro/aws-auth-command/releases/download/v0.1.0/awx-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "SHA256_LINUX_ARM"
+    else
+      url "https://github.com/soranjiro/aws-auth-command/releases/download/v0.1.0/awx-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "SHA256_LINUX_INTEL"
+    end
+  end
+
+  def install
+    bin.install "awx"
+  end
+
+  test do
+    assert_match "awx", shell_output("#{bin}/awx --version")
+  end
+end
